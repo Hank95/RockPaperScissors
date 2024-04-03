2bb72ad00cad97650426b59742380e123ff85c30
Henry Pendleton <hhpendleton@gmail.com>
2024-04-03 10:49:49 -0400

    started game

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -10,24 +10,48 @@ import SwiftUI
 struct ContentView: View {
     let choices = ["rock", "paper", "scissors"]
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var resultTitle = "Pick an option"
+    @State private var points = 0
+    @State private var round = 1
     
     
     var body: some View {
-        VStack {
-            ForEach(choices, id: \.self ){ choice in
-                Button(action: buttonAction){
-                    Text(choice)
-                        .background(.red)
-                        .padding()
-                        .cornerRadius(10)
+        ZStack{
+            LinearGradient(colors:[.red, .black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+                Text(resultTitle)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .font(.largeTitle.weight(.semibold))
+                ForEach(choices.indices, id: \.self ){ index in
+                    Button(action:{ self.buttonAction(index)}){
+                        Text(choices[index].capitalized)
+                        
+                            .font(.system(size: 24))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .padding()
+                        
+                    }
+                    .frame(width: 200)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
                 }
+                Spacer()
+                Spacer()
+                
             }
-            
+            .padding()
         }
-        .padding()
     }
     
-    func buttonAction(){
+    func buttonAction(_ index:Int){
+        if(index == correctAnswer){
+            
+        }
         correctAnswer = Int.random(in: 0...2)
         print(correctAnswer)
     }
