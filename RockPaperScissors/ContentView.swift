9b17659a5703f8400b52a98c98e7f39b3757ecb7
Henry Pendleton <hhpendleton@gmail.com>
2024-04-03 10:20:27 -0400

    init

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let choices = ["rock", "paper", "scissors"]
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(choices, id: \.self ){ choice in
+                Button(action: buttonAction){
+                    Text(choice)
+                        .background(.red)
+                        .padding()
+                        .cornerRadius(10)
+                }
+            }
+            
         }
         .padding()
+    }
+    
+    func buttonAction(){
+        correctAnswer = Int.random(in: 0...2)
+        print(correctAnswer)
     }
 }
 
